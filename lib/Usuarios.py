dad95bf1539d5ad39499038600c9918981e925f0
BabyMage <lucascfoliveira2025@gmail.com>
2025-10-29 15:13:33 -0300

    Class Endereco

---
+++ lib/Usuarios.py
@@ -0,0 +1,54 @@
+class Usuarios():
+    def __init__(self, nome, CPF, emial, senha, telefone):
+        self.nome = nome
+        self.cpf = CPF
+        self.email = emial
+        self.senha = senha
+        self.telefone = telefone
+
+    def setNome(self, novo_nome):
+        self.nome = novo_nome
+
+        return self.nome
+    
+    def getNOme(self):
+        return self.nome
+
+    
+    
+    def setCPF(self, novo_CPF):
+        self.cpf = novo_CPF
+        
+        return self.cpf
+    
+    def getCPF(self):
+        return self.cpf
+    
+
+
+    def setEmail(self, novo_emial):
+        self.email = novo_emial
+
+        return self.email
+    
+    def getEmail(self):
+        return self.email
+    
+
+
+    def setSenha(self, nova_senha):
+        self.senha = nova_senha
+
+        return self.senha
+    
+    def getSenha(self):
+        return self.senha
+    
+
+    def setTelefone(self, novo_telefone):
+        self.telefone = novo_telefone
+
+        return self.telefone
+    
+    def getTelefone(self):
+        return self.telefone
--- lib/teste.py
+++ lib/teste.py
@@ -1,1 +1,63 @@
-print('Hello World')
+class Endereco():
+    def __init__(self, apelido, rua, numero, complimento, pontoReferencia, bairro, cidade):
+        self.apelido = apelido
+        self.rua = rua
+        self.numero = numero
+        self.complimento = complimento
+        self.p_referencia = pontoReferencia
+        self.bairro = bairro
+        self.cidade = cidade
+
+###############################################################
+
+    def setApelido(self, novoApelido):
+        self.apelido = novoApelido
+        return novoApelido
+
+    def getApelido(self):
+        return self.apelido
+    
+###############################################################
+    
+    def setRua(self, novaRua):
+        self.rua = novaRua
+        return novaRua
+    
+    def getRua(self):
+        return self.rua
+
+###############################################################
+
+    def setNumero(self, novoNumero):
+        self.numero = novoNumero
+        return self.numero
+    
+    def getNumero(self):
+        return self.numero
+    
+###############################################################
+    
+    def setComplimento(self, novocomlimento):
+        self.complimento = novocomlimento
+        return self.complimento
+    
+    def getComplimento(self):
+        return self.complimento
+
+###############################################################
+
+    def setBairro(self, novoBairro):
+        self.bairro = novoBairro
+        return self.bairro
+    
+    def getBairro(self):
+        return self.bairro
+    
+###############################################################
+
+    def setCidade(self, novaCidade):
+        self.cidade = novaCidade
+        return self.cidade
+    
+    def getCidade(self):
+        return self.cidade
