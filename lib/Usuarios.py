ba2f95c7fbce9e186048128ad3f6983eb959a292
BabyMage <lucascfoliveira2025@gmail.com>
2025-10-31 23:44:31 -0300

    Update Usuarios.py
    Coloquei as Linhas divisorias igual fiz no outro arquivo. Não alterei nada.

--- lib/Usuarios.py
+++ lib/Usuarios.py
@@ -8,47 +8,43 @@ class Usuarios():
 
     def setNome(self, novo_nome):
         self.nome = novo_nome
-
         return self.nome
-    
+
     def getNOme(self):
         return self.nome
-
     
+####################################################### 
     
     def setCPF(self, novo_CPF):
         self.cpf = novo_CPF
-        
         return self.cpf
-    
+        
     def getCPF(self):
         return self.cpf
     
-
+#######################################################
 
     def setEmail(self, novo_emial):
         self.email = novo_emial
-
         return self.email
-    
+
     def getEmail(self):
         return self.email
     
-
+#######################################################    
 
     def setSenha(self, nova_senha):
         self.senha = nova_senha
-
         return self.senha
-    
+        
     def getSenha(self):
         return self.senha
     
+#######################################################
 
     def setTelefone(self, novo_telefone):
         self.telefone = novo_telefone
-
         return self.telefone
-    
+
     def getTelefone(self):
         return self.telefone
