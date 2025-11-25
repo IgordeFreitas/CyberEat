5662729550ed3f75b668ad7315b84878be7d2e13
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:29:34 -0300

    Update deleteEnderecos.py

--- model/Enderecos/deleteEnderecos.py
+++ model/Enderecos/deleteEnderecos.py
@@ -1,6 +1,6 @@
 def deleteEnderecos(conectionDB, idEnderecos):
     cursor = conectionDB.cursor(dictionary = True)
-    cursor.execute('DELETE FROM Enderecos WHERE id_Enderecos = %s', (idEnderecos))
+    cursor.execute('DELETE FROM enderecos WHERE id_Enderecos = %s', (idEnderecos))
     cursor.fetchall()
     cursor.close()
     return cursor.rowcount
