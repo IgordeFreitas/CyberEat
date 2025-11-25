166adfac590a3985d7cd533f2409ed854d528b40
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:32:58 -0300

    Update postRestaurantes.py

--- model/Restaurantes/postRestaurantes.py
+++ model/Restaurantes/postRestaurantes.py
@@ -1,1 +1,6 @@
-
+def insertRestaurantes(conectionDB, id_produto , id_usuarios , id_endereco , nome_restaurante, categoria ):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('INSERT INTO restaurantes (id_produto , id_usuarios , id_endereco , nome_restaurante, categoria ) VALUES(%s, %s)', (id_produto , id_usuarios , id_endereco , nome_restaurante, categoria ))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
