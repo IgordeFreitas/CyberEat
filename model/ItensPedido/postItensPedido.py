8d18a21e51a5fd01fb4088e98e810740a1ed0d02
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:20:21 -0300

    Update postItensPedido.py

--- model/ItensPedido/postItensPedido.py
+++ model/ItensPedido/postItensPedido.py
@@ -1,1 +1,6 @@
-
+def insertItensPedido(conectionDB, id_pedidos , nome_item, quantidade, preco_unitario):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('INSERT INTO ItensPedido (nome, email, senha, telefone) VALUES(%s, %s, %s)', (id_pedidos, nome_item, quantidade, preco_unitario))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
