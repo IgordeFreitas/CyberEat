c10d0ebd3a76e020419258f29b2d4663c4b7eede
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:17:55 -0300

    Update postEntregas.py

--- model/Entregas/postEntregas.py
+++ model/Entregas/postEntregas.py
@@ -1,1 +1,6 @@
-
+def insertEntregas(conectionDB, id_endereco,data_entrega):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('INSERT INTO entregas (id_endereco, data_entrega ) VALUES(%s)', (id_endereco, data_entrega))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
