60e3cba7ed3847d64465e64023593526405f1c36
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:31:11 -0300

    Update deleteRestaurantes.py

--- model/Restaurantes/deleteRestaurantes.py
+++ model/Restaurantes/deleteRestaurantes.py
@@ -1,1 +1,6 @@
-
+def deleteRestaurantes(conectionDB, idRestaurantes):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('DELETE FROM restaurantes WHERE id_Restaurantes = %s', (idRestaurantes))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
