b246c42e469a28e503c23bfde6065ca5a6b88956
BabyMage <lucasfoliveira2025@gmail.com>
2025-11-25 11:29:44 -0300

    Erro de importação corrigido

--- controller/controllerUsuarios.py
+++ controller/controllerUsuarios.py
@@ -1,10 +1,10 @@
 import mysql.connector
 from config.dbConfig import getConnection
-from model.usuarios.getUsuarios import queryUsuarios
-from model.usuarios.postUsuarios import insertUsuarios
-from model.usuarios.deleteUsuario import deleteUsuarios
+from model.Usuarios.getUsuarios import queryUsuarios
+from model.Usuarios.postUsuarios import insertUsuarios
+from model.Usuarios.deleteUsuario import deleteUsuarios
 
-from lib import * 
+############################################################################
 
 
 def consultarUsuarios():
