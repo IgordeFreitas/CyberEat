8d19a6b064c27a48dff098381eaa7d2c3043dfe4
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:24:53 -0300

    Update deletePedidos.py

--- model/Pedidos/deletePedidos.py
+++ model/Pedidos/deletePedidos.py
@@ -1,1 +1,6 @@
-
+def deletePedidos(conectionDB, idPedidos):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('DELETE FROM pedidos WHERE id_Pedidos = %s', (idPedidos))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
