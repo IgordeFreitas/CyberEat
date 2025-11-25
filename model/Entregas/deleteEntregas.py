28162636eb15c6cd3e46a1c19dc34321a6176749
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:29:24 -0300

    Update deleteEntregas.py

--- model/Entregas/deleteEntregas.py
+++ model/Entregas/deleteEntregas.py
@@ -1,6 +1,6 @@
 def deleteEntregas(conectionDB, idEntregas):
     cursor = conectionDB.cursor(dictionary = True)
-    cursor.execute('DELETE FROM Entregas WHERE id_Entregas = %s', (idEntregas))
+    cursor.execute('DELETE FROM entregas WHERE id_Entregas = %s', (idEntregas))
     cursor.fetchall()
     cursor.close()
     return cursor.rowcount
