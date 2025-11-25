1f81f17f2f87e9d76570d28b5b1f960c0e90c967
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:27:39 -0300

    Update deleteProdutos.py

--- model/Produtos/deleteProdutos.py
+++ model/Produtos/deleteProdutos.py
@@ -1,1 +1,6 @@
-
+def deleteProdutos(conectionDB, idUsuario):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('DELETE FROM Produtos WHERE id_Produtos = %s', (idProdutos))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
