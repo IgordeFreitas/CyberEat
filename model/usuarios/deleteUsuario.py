435fa516fd33ffc899f97c0b94e345ee661fb1b2
BabyMage <lucasfoliveira2025@gmail.com>
2025-11-25 09:50:26 -0300

    Ajustes de Padrão e criação de novas pastas na model

--- model/usuarios/deleteUsuario.py
+++ model/usuarios/deleteUsuario.py
@@ -1,6 +1,6 @@
 def deleteUsuarios(conectionDB, idUsuario):
     cursor = conectionDB.cursor(dictionary = True)
-    cursor.execute('DELETE FROM usuarios WHERE id_usuarios = %s', (idUsuario,))
+    cursor.execute('DELETE FROM usuarios WHERE id_usuarios = %s', (idUsuario))
     cursor.fetchall()
     cursor.close()
     return cursor.rowcount
