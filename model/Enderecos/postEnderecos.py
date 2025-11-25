4ef3a5485c50375eddbb75b35dce59b5dc453261
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:13:43 -0300

    Update postEnderecos.py

--- model/Enderecos/postEnderecos.py
+++ model/Enderecos/postEnderecos.py
@@ -1,6 +1,6 @@
-def insertEnderecos(conectionDB, , bairro):
+def insertEnderecos(conectionDB,id_usuarios , bairro):
     cursor = conectionDB.cursor(dictionary = True)
-    cursor.execute('INSERT INTO Enderecos ( , bairo) VALUES(%s, %s, %s, %s)', ( ,bairro ))
+    cursor.execute('INSERT INTO Enderecos ( id_usuarios, bairo) VALUES(%s)', ( id_usuarios,bairro ))
     cursor.fetchall()
     cursor.close()
     return cursor.rowcount
