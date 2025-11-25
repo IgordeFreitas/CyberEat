a4b7621d6aabb98fa733e0b2166cdb48e2e28380
BabyMage <lucascfoliveira2025@gmail.com>
2025-11-25 10:16:57 -0300

    Update postEnderecos.py

--- model/Enderecos/postEnderecos.py
+++ model/Enderecos/postEnderecos.py
@@ -1,6 +1,6 @@
-def insertEnderecos(conectionDB,id_usuarios , bairro):
+def insertEnderecos(conectionDB, id_usuarios, bairro):
     cursor = conectionDB.cursor(dictionary = True)
-    cursor.execute('INSERT INTO Enderecos ( id_usuarios, bairo) VALUES(%s)', ( id_usuarios,bairro ))
+    cursor.execute('INSERT INTO Enderecos (id_usuarios, bairo) VALUES(%s, %s)', (id_usuarios, bairro )
     cursor.fetchall()
     cursor.close()
     return cursor.rowcount
