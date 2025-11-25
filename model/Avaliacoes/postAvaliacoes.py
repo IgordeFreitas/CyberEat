be5faeea94215b2bab8a0489f2d3e1805bf07562
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:09:35 -0300

    Update postAvaliacoes.py

--- model/Avaliacoes/postAvaliacoes.py
+++ model/Avaliacoes/postAvaliacoes.py
@@ -1,6 +1,6 @@
-def insertAvaliacoes(conectionDB, nome, email, senha, telefone):
+def insertAvaliacoes(conectionDB, id_pedidos, nota, comentario):
     cursor = conectionDB.cursor(dictionary = True)
-    cursor.execute('INSERT INTO usuarios (nome, email, senha, telefone) VALUES(%s, %s, %s, %s)', (nome, email, senha, telefone))
+    cursor.execute('INSERT INTO avaliacoes (nota, comentario) VALUES(%s, %s)', (nota, comentario))
     cursor.fetchall()
     cursor.close()
     return cursor.rowcount
