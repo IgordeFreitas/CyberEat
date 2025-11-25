40a7bdb8ecfdabc207c9cbc3436b35be2eaf7581
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:26:46 -0300

    Update postPedidos.py

--- model/Pedidos/postPedidos.py
+++ model/Pedidos/postPedidos.py
@@ -1,1 +1,6 @@
-
+def insertPedidos(conectionDB, id_restaurantes , id_usuarios , id_endereco , id_pagamento, id_entrega ):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('INSERT INTO Pedidos (id_restaurantes, id_usuarios, id_endereco, id_pagamento, id_entrega ) VALUES(%s, %s, %s, %s)', (id_restaurantes, id_usuarios, id_endereco, id_pagamento, id_entrega))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
