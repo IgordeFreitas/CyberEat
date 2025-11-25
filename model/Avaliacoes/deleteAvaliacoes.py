4696a3013396c4ff2a850f1faca41ae6ecbc769b
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:04:41 -0300

    Update deleteAvaliacoes.py

--- model/Avaliacoes/deleteAvaliacoes.py
+++ model/Avaliacoes/deleteAvaliacoes.py
@@ -1,1 +1,6 @@
-
+def deleteAvaliacoes(conectionDB, idAvaliacoes):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('DELETE FROM avaliacoes WHERE id_avaliacoes = %s', (idAvaliacoes))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
