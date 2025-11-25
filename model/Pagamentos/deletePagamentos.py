e6e84143da7f174295fce356b57e1f7cee021af6
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:21:56 -0300

    Update deletePagamentos.py

--- model/Pagamentos/deletePagamentos.py
+++ model/Pagamentos/deletePagamentos.py
@@ -1,1 +1,6 @@
-
+def deletePagamentos(conectionDB, idPagamentos):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('DELETE FROM pagamentos WHERE id_pagamentos = %s', (idPagamentos))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
