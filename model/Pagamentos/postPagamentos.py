6ffe5acaf8a9fb98096620bd81d9aa4601bd0a5e
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:23:45 -0300

    Update postPagamentos.py

--- model/Pagamentos/postPagamentos.py
+++ model/Pagamentos/postPagamentos.py
@@ -1,1 +1,6 @@
-
+def insertPagamentos(conectionDB, tipo_pagamento, status_pagamento, valor_total):
+    cursor = conectionDB.cursor(dictionary = True)
+    cursor.execute('INSERT INTO pagamentos ( tipo_pagamento, status_pagamento, valor_total) VALUES(%s, %s, %s)', ( tipo_pagamento, status_pagamento, valor_total))
+    cursor.fetchall()
+    cursor.close()
+    return cursor.rowcount
