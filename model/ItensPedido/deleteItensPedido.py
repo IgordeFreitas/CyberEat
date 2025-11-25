154e5bf0e40ee89c2c9203a28ba58b91796a2b49
Pedro Augusto Xavier Machado <pedroaugustoxmachado@gmail.com>
2025-11-25 10:29:15 -0300

    Update deleteItensPedido.py

--- model/ItensPedido/deleteItensPedido.py
+++ model/ItensPedido/deleteItensPedido.py
@@ -1,6 +1,6 @@
 def deleteItensPedido(conectionDB, idItensPedido):
     cursor = conectionDB.cursor(dictionary = True)
-    cursor.execute('DELETE FROM ItensPedido WHERE id_ItensPedido = %s', (idItensPedido))
+    cursor.execute('DELETE FROM itensPedido WHERE id_ItensPedido = %s', (idItensPedido))
     cursor.fetchall()
     cursor.close()
     return cursor.rowcount
