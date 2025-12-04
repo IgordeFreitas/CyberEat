b539d4527c99712e759def6a448827b2df6fcdac
pedro-augusto-xavier <pedroaugustoxmachado@gmail.com>
2025-12-04 10:16:23 -0300

    continuaçao dos updates

--- index.py
+++ index.py
@@ -185,6 +185,12 @@ def deletarPagamentos(id_pagamento: int = Body(embed = True)):
     controllerPagamentos.deletarPagamentos(id_pagamento)
 
 
+@app.patch("/pagamentos")
+
+def alterarPagamentos(tipo_pagamento: str = Body(embed = True) , status_pagamento: str = Body(embed = True), valor_total: str = Body(embed = True), idpagamentos: int = Body(embed = True)):
+    return     controllerPagamentos.alterarPagamentos(tipo_pagamento, status_pagamento,  valor_total, idpagamentos)
+
+
 ##################################################################
 
 
